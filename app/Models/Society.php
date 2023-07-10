7279ff00fffbdf585230346c2288ae447fd5c271
WindeusK <129594654+WindeusK@users.noreply.github.com>
2023-07-10 23:18:42 +0700

    make society model

---
+++ app/Models/Society.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Society extends Model
+{
+    public $guarded = ['id'];
+
+    protected $timestamps = false;
+    protected $fillable = [
+        'name',
+        'born_date',
+        'gender',
+        'address',
+        'token',
+        'regional_id',
+        'regional_province',
+        'regional_district'
+    ];
+}
