4112380e9aec8fedb3d88c981981017ef0b4fc62
WindeusK <129594654+WindeusK@users.noreply.github.com>
2023-07-11 18:45:24 +0700

    add all() function @ test controller

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,8 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Society;
+
 class TestController extends Controller
 {
+    public function all ()
+    {
+        return Society::all();
+    }
     public function test_new_data (Request $request)
     {
         // TODO: make factory
